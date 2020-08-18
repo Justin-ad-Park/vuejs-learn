9ef8d35d01f584ff8a5fddcd4cef10f3fcef6984
Justin Park <justin@Justins-Mac-Pro.local>
2020-08-18 22:12:57 +0900

    vue components

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
